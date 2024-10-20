068ecdd89359646063c0e0989c6c797939828e24
Roman <romansrgvch@outlook.com>
2024-10-20 21:55:16 +0300

    configure project

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+// import { Header } from '@/shared/components/shared';
+import type { Metadata } from "next";
+// import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: "TonLink | Main",
+};
+
+export default function HomeLayout({
+  children,
+  modal,
+}: Readonly<{
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}>) {
+  return (
+    <main className="min-h-screen">
+      {/* <Header /> */}
+      {children}
+      {modal}
+    </main>
+  );
+}
